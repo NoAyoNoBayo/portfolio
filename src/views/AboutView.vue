a2d8fd5c1bc5cfe2e107fc08b77aa6c683e83d68
Dimitrios Kapouranis <d.kapouranis@adrestia.eu>
2024-02-28 15:06:49 +0200

    feat:further development of about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -13,18 +13,26 @@
         Hi! I am Dimitrios, nice to have you here!
       </div>
       <div class="flex max-w-20rem" style="color: var(--neutral);">
-        I am a full stack developer that loves designing, building and finally delivering software.
-        So I have decided to build this portfolio to make different tools for free!
+        I am a full stack developer that loves designing, building and delivering software.
+        So I have decided to build this portfolio to develop and offer different tools for free!
         Hopefully this portfolio will continue to grow as I do! </div>
     </div>
     <div class="flex flex-column align-items-center">
       <div class="flex align-items-center gap-3 text-3xl p-4" style="color: var(--light);">
-        <i class="pi pi-code text-3xl" />
-        Technologies and Frameworks
+        <div class="flex flex-column align-items-center">
+          <div class="flex flex-row align-items-center gap-3">
+            <i class="pi pi-code text-3xl" />
+            Technologies and Frameworks
+          </div>
+          <div class="text-xl" style="color: var(--neutral);">
+            A list of development technologies I love to work with
+          </div>
+        </div>
       </div>
       <div class="flex flex-row flex-wrap gap-4 p-4" style="color: var(--light);">
         <Card class="max-w-20rem">
           <template #title>
+            <i class="pi pi-desktop text-lg" />
             Frontend
           </template>
           <template #subtitle>
@@ -35,11 +43,25 @@
             </div>
           </template>
           <template #content>
-            content
+            <div class="flex flex-column gap-5 p-3">
+              <a href="https://vuejs.org/">
+                <div class="flex flex-row align-items-center text-2xl gap-3" style="color: var(--light);">
+                  <img src="/src/assets/vuejs.png" width="60" />
+                  VueJS
+                </div>
+              </a>
+              <a href="https://angular.io/">
+                <div class="flex flex-row align-items-center text-2xl gap-3" style="color: var(--light);">
+                  <img src="/src/assets/angularjs.png" width="60" />
+                  Angular
+                </div>
+              </a>
+            </div>
           </template>
         </Card>
         <Card class="max-w-20rem">
           <template #title>
+            <i class="pi pi-wrench text-lg" />
             Backend
           </template>
           <template #subtitle>
@@ -50,22 +72,43 @@
             </div>
           </template>
           <template #content>
-            content
+            <div class="flex flex-column gap-5 p-3">
+              <a href="https://go.dev/">
+                <div class="flex flex-row align-items-center text-2xl gap-3" style="color: var(--light);">
+                  <img src="/src/assets/golang.png" width="60" />
+                  Go
+                </div>
+              </a>
+            </div>
           </template>
         </Card>
         <Card class="max-w-20rem">
           <template #title>
-            Databases and Repositories
+            <i class="pi pi-database text-lg" />
+            Repositories
           </template>
           <template #subtitle>
             <div class="flex">
               <div>
-                Technologies that made data storage and accessing data easier and faster!
+                Technologies that made data access and storage easier and faster!
               </div>
             </div>
           </template>
           <template #content>
-            content
+            <div class="flex flex-column gap-5 p-3">
+              <a href="https://postgresql.org/">
+                <div class="flex flex-row align-items-center text-2xl gap-3" style="color: var(--light);">
+                  <img src="/src/assets/postgres.png" width="60" />
+                  PostgreSQL
+                </div>
+              </a>
+              <a href="https://redis.io/">
+                <div class="flex flex-row align-items-center text-2xl gap-3" style="color: var(--light);">
+                  <img src="/src/assets/redis.svg" width="60" />
+                  Redis
+                </div>
+              </a>
+            </div>
           </template>
         </Card>
       </div>
