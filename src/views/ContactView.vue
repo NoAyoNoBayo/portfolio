bc85f8ee86d1e8677c686175440c799f912fa623
Dimitrios Kapouranis <d.kapouranis@adrestia.eu>
2024-06-27 12:14:54 +0300

    feat: gh-pages adaptation

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -11,27 +11,27 @@
         <div class="flex gap-3 flex-column">
           <div class="flex flex-column">
             <InputText placeholder="First Name" type="text" v-model="email.firstName" />
-            <small class="m-0 p-0" v-for="error in v$.firstName.$errors" :key="error.uid" style="color: red;">
+            <small class="m-0 p-0" v-for="error in v$.firstName.$errors" :key="error.$uid" style="color: red;">
               Required
             </small>
           </div>
           <div class="flex flex-column">
             <InputText placeholder="Last Name" type="text" v-model="email.lastName" />
-            <small v-for="error in v$.lastName.$errors" :key="error.uid" style="color: red;">Required</small>
+            <small v-for="error in v$.lastName.$errors" :key="error.$uid" style="color: red;">Required</small>
           </div>
           <div class="flex flex-column">
             <InputText placeholder="Email Address" type="email" v-model="email.emailAddress" />
-            <small v-for="error in v$.emailAddress.$errors" :key="error.uid" style="color: red;">Required</small>
+            <small v-for="error in v$.emailAddress.$errors" :key="error.$uid" style="color: red;">Required</small>
           </div>
         </div>
         <div class="flex flex-column">
           <InputText placeholder="Subject" type="text" v-model="email.subject" />
-          <small v-for="error in v$.subject.$errors" :key="error.uid" style="color: red;">Required</small>
+          <small v-for="error in v$.subject.$errors" :key="error.$uid" style="color: red;">Required</small>
         </div>
         <div class="flex flex-column">
           <Textarea placeholder="Message" type="text" maxlength="150" rows="5" cols="30" style="resize: none;"
             v-model="email.body" />
-          <small v-for="error in v$.body.$errors" :key="error.uid" style="color: red;"> Required </small>
+          <small v-for="error in v$.body.$errors" :key="error.$uid" style="color: red;"> Required </small>
         </div>
         <Button label="Send" type="submit" icon="pi pi-send"
           style="background: var(--lighter_dark); color: var(--light);border-color: var(--dark);" />
