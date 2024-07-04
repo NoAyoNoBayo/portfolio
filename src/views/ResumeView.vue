acc7203743de11a2ba70b380ecc7da3cdcdfe4b8
Dimitrios Kapouranis <d.kapouranis@adrestia.eu>
2024-07-04 04:34:28 +0300

    fix: change to load cv and photo from public

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -7,9 +7,9 @@
       Read or download my resume.
     </div>
     <div>
-      <Image src="ResumePhoto.png" alt="Image" width="250" preview />
+      <Image src="cv/ResumePhoto.png" alt="Image" width="250" preview />
     </div>
-    <a href="kapouranis-dimitrios.pdf">
+    <a href="cv/kapouranis-dimitrios.pdf">
       <Button label="Download" icon="pi pi-download" style="color: var(--light); background: var(--dark);" />
     </a>
   </div>
